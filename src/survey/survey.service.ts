2874a545d43ff34b6d13709c8ec7398c59d1cf96
ikjingo <rhdlrwls55@naver.com>
2023-11-26 12:08:21 +0900

    설문지 API (update, remove) ResourceNotFound 예외처리 추가

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Survey } from './entities/survey.entity';
 import { CreateSurveyInput, UpdateSurveyInput } from './dto/survey.input';
+import { ResourceNotFoundException } from 'src/common/exceptions/resource-not-found.exception';
+import { Constants } from 'src/common/constants';
 
 @Injectable()
 export class SurveyService {
@@ -18,14 +20,33 @@ export class SurveyService {
 
     async update(updateSurveyInput: UpdateSurveyInput): Promise<Survey> {
         const { id, ...updateData } = updateSurveyInput;
+
+        const survey = await this.findOneById(id);
+        // 조회한 설문지가 존재하지 않으면, ResourceNotFoundException을 발생
+        if (!survey) {
+            throw new ResourceNotFoundException(Constants.RESOURCE_SURVEY, id);
+        }
+
         await this.surveyRepository.update(id, updateData);
-        return this.surveyRepository.findOne({ where: { id } });
-      }
+        survey.title = updateData.title
+        survey.description = updateData.description
+
+        return survey
+    }
     
-      async remove(id: number): Promise<boolean> {
+    async remove(id: number): Promise<boolean> {
         const result = await this.surveyRepository.delete(id);
-        return result.affected > 0;
-      }
+        // 삭제하려는 설문지 ID가 존재하지 않으면, ResourceNotFoundException을 발생
+        if (result.affected === 0) {
+            throw new ResourceNotFoundException(Constants.RESOURCE_SURVEY, id);
+        }
+
+        return true
+    }
+
+    async findOneById(id: number): Promise<Survey | undefined> {
+        return this.surveyRepository.findOne({ where: { id } })
+    }
 
     async findAll(): Promise<Survey[]> {
         return this.surveyRepository.find();
